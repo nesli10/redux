5e0fc0a120afb3acc7b2fb3c0286cba73ffb9ba4
neslişah ebral <nestay2001@gmail.com>
2022-10-04 16:23:16 +0300

    mini redux project completed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
-
+import configureStore from "./redux/reducers/configureStore";
+import {Provider} from "react-redux";
+const store = configureStore();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
-    <App />
+  <Provider store={store}>
+     <App />
+  </Provider>
+   
   
 );
 
